1a7e50219d47095aeb7d2c10af7c26c56b51ae98
Asimjumani <51481782+Asimjumani@users.noreply.github.com>
2020-06-15 21:37:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,206 @@
+//Chapter # 01   Task 01
+// alert('Welcome to our website')
+
+//Chapter # 01   Task 02
+// alert('Error! Please enter a valid password.')
+
+//Chapter # 01   Task 03
+// alert('Welcome to JS Land.... \nHappy Coding!')
+
+
+// //Chapter # 01   Task 04
+//  alert('Welcome to JS Land...')
+//  alert('Happy Coding!')
+
+
+ //Chapter # 01   Task 05
+//  alert("Hello...I can run JS through my web browser's console")
+
+
+//Chapter # 01   Task 06
+// alert("Welcome to CNC HOUSE")
+
+
+//Chapter # 01   Task 07
+// Answer is d. Body (aftter your page's HTML)
+
+
+
+
+
+
+
+
+//Chapter # 02   Task 01
+// var username = "jumani"
+
+//Chapter # 02   Task 02
+// var myName = "Muhammad Asim"
+
+
+//Chapter # 02   Task 03
+// var message = "Hello World"
+// alert(message)
+
+
+//Chapter # 02   Task 04
+// var studentName = 'Jhone Doe'
+// alert(studentName )
+// var age = '15 years old'
+// alert(age)
+// var education = ' Certified Mobile Appliacation Development'
+// alert(education)
+
+
+//Chapter # 02   Task 05
+// var eat ='PIZZA \n PIZZ \n PIZ \n PI \n P'
+// alert(eat)
+
+
+//Chapter # 02   Task 06
+// var email
+// email =" asimjumani@gmail.com"
+// alert('My email address is ' + email)
+
+
+//Chapter # 02   Task 07
+// var book
+// book = "A smarter way to learn JavaScript"
+// alert ('I am trying to leran from the Book ' + book )
+
+
+
+//Chapter # 02   Task 08
+// var display = ("Yah! I can write HTML content through JavaScript")
+// document.write(display)
+
+
+
+//Chapter # 02   Task 09
+// var ajeeeb = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert (ajeeeb)
+
+
+
+
+
+
+
+
+
+//Chapter # 03   Task 01
+// var age = 30
+// alert ('I am ' + age + ' years old')
+
+
+//Chapter # 03   Task 02
+// var  S = "1"
+// var N = S++
+// alert ('You have visited this site' + N + " times") 
+
+
+//Chapter # 03   Task 03
+// var birthYear
+// birthYear = 1990
+// document.write("My birth year is " + birthYear  + "<br>  Data type of my declared variable is number")
+
+
+
+//Chapter # 03   Task 04
+// var vistorName = prompt('Enter your name');
+// var productTiltle = prompt('Enter your product');
+// var quantity = prompt('How mamy product you order'); 
+
+// document.write (vistorName + " ordered " + quantity + productTiltle + " on XYZ Clothing store.")
+
+
+
+
+
+
+
+
+
+//Chapter # 04   Task 01
+// var variable1 = 'Assalam o alaikum' , 
+//     variable2 = "How are You?"
+//     Variable3 = "Alhamdulillah!"
+
+
+
+
+//Chapter # 04   Task 02
+// LEGAL
+// 01 .  Can contain only letters
+// 02 .  After letter contains numbers too.\
+// 03 .  Capital letters are allowed
+// 04 .  camelCase is better than to use
+// 05 .  dollar signs and underscores are allowed
+
+// ILLEGAL
+// 01 .  Can't enclosed it in quotation mark
+// 02 .  Can't be only a  numbers.
+// 03 .  Can't start with in a numbers.
+// 04 .  Can't contain any spaces.
+// 05 .  Can't be any of JavaScript's keywords
+
+
+//Chapter # 04   Task 03
+// document.write('<h1> Rules for naming JS variables </h1>' + '<br> Variable names can only contain letters , $ , underscores   For example $my_1stVariable'  +  '<br> Variables must begin with a letters , $ , _. For example $name, _name or name '  + 
+//                 '<br>  Variable names are case sensitive'  +  '<br> Variable names should not be JS keywords')
+
+
+
+
+
+
+
+
+
+
+//Chapter # 05   Task 01
+// var num01 = prompt("Enter first number");
+// var num02 = prompt("Enter second number");
+// var fn = parseInt(num01)
+// var fn02 = parseInt(num02)
+// var ans = fn + fn02
+// document.write('Sum of ' + num01 + " and " + num02 + ' is ' +  ans)
+
+
+
+//Chapter # 05   Task 02
+// var num01 = prompt("Enter first number");
+// var num02 = prompt("Enter second number");
+// var fn = parseInt(num01)
+// var fn02 = parseInt(num02)
+// var ans = fn - fn02
+// document.write('Subtract from ' + num01 + " to " + num02 + ' is ' +  ans)
+
+// var num01 = prompt("Enter first number");
+// var num02 = prompt("Enter second number");
+// var fn = parseInt(num01)
+// var fn02 = parseInt(num02)
+// var ans = fn * fn02
+// document.write('Multiply ' + num01 + " and " + num02 + ' is ' +  ans)
+
+
+
+// var num01 = prompt("Enter first number");
+// var num02 = prompt("Enter second number");
+// var fn = parseInt(num01)
+// var fn02 = parseInt(num02)
+// var ans = fn / fn02
+// document.write('Division of ' + num01 + " and " + num02 + ' is ' +  ans)
+
+
+// var num01 = prompt("Enter first number");
+// var num02 = prompt("Enter second number");
+// var fn = parseInt(num01)
+// var fn02 = parseInt(num02)
+// var ans = fn % fn02
+// document.write('Modulus of ' + num01 + " and " + num02 + ' is ' +  ans)
+
+
+
+
+//Chapter # 05   Task 03
